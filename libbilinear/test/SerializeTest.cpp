ff15768e608da6fe14573630bd31c8b033168d04
Alin Tomescu <alinush@mit.edu>
2018-11-22 15:23:39 -0500

    updated serialization test

--- libbilinear/test/SerializeTest.cpp
+++ libbilinear/test/SerializeTest.cpp
@@ -32,48 +32,52 @@ using namespace Bilinear;
  * RELIC has some bugs in gt_size_bin(): it gets stuck in an infinte loop if given a zero or one gt_t.
  * This test suite makes sure g1_t, g2_t and bn_t don't have similar problems.
  */
-void testRandomSerialize() {
-    GTT tr, tz, tu;
-    gt_rand(tr);
+void testIdentitySerialize() {
+    stringstream ss;
+
+    GTT tz, tu;
     gt_zero(tz);
     gt_set_unity(tu);
-
-    stringstream ss;
-    ss << "Random GTT: " << tr << endl;
     ss << "Zero GTT: " << tz << endl;
     ss << "Unity GTT: " << tu << endl;
 
-    logtrace << endl << ss.str() << endl;
-    ss.clear();
-
-    G1T g1r, g1i;
-    g1_rand(g1r);
+    G1T g1i;
     g1_set_infty(g1i);
-
-    ss << "Random G1T: " << g1r << endl;
     ss << "Infty G1T: " << g1i << endl;
-    logtrace << endl << ss.str() << endl;
-    ss.clear();
 
-    G2T g2r, g2i;
-    g2_rand(g2r);
+    G2T g2i;
     g2_set_infty(g2i);
-
-    ss << "Random G2T: " << g2r << endl;
     ss << "Infty G2T: " << g2i << endl;
-    logtrace << endl << ss.str() << endl;
-    ss.clear();
 
-    BNT b2r, b2z, b2u;
-    bn_rand(b2r, BN_POS, BN_PRECI);
+    BNT b2z, b2u;
     bn_zero(b2z);
     bn_set_dig(b2u, 1);
-
-    ss << "Random BNT: " << b2r << endl;
     ss << "Zero BNT: " << b2z << endl;
     ss << "One BNT: " << b2u << endl;
+
+    loginfo << endl << ss.str() << endl;
+}
+
+void testRandomSerialize() {
+    stringstream ss;
+
+    GTT tr;
+    gt_rand(tr);
+    ss << "Random GTT: " << tr << endl;
+
+    G1T g1r;
+    g1_rand(g1r);
+    ss << "Random G1T: " << g1r << endl;
+
+    G2T g2r;
+    g2_rand(g2r);
+    ss << "Random G2T: " << g2r << endl;
+
+    BNT b2r;
+    bn_rand(b2r, BN_POS, BN_PRECI);
+    ss << "Random BNT: " << b2r << endl;
+
     logtrace << endl << ss.str() << endl;
-    ss.clear();
 }
 
 template<class T>
@@ -116,7 +120,10 @@ int BilinearAppMain(const Library& lib, const std::vector<std::string>& args) {
     (void)args;
     (void)lib;
 
-    logtrace << "Serialization test..." << endl;
+    loginfo << "Serialization test..." << endl;
+
+    testIdentitySerialize();
+
     int n = 500;
     for(int i = 0; i < n; i++) {
         if(i % 100 == 0) {
